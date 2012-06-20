5eac89ffac97b709c8ef3bc7ad03e467c9300d8e
Rajat Mittal <lifeinafolder@gmail.com>
2012-06-20 18:19:29 +0530

    [Form] Refactor.

--- plugins/form/Form.js
+++ plugins/form/Form.js
@@ -9,50 +9,80 @@
  * @group Plugin
  */
 JX.install('Form', {
-  construct: function(uri){
+  construct: function(uri, validationFns){
     this.setUri(uri);
+    this.setValidationFns(validationFns);
   },
-  events:['start','done','invalid'],
+  events:['start','done','fail','invalid','valid'], // TODO: invoke 'fail'
   members: {
     _cbk: function(response){
       this.invoke('done',{a:5});
     },
     validate: function(){
-      return true;
-    },
-    submit: function(){
-      this.invoke('start');
-      var isValid = this.validate();
-      if (!isValid){
-        this.invoke('invalid');
+      validationFns = this.getValidationFns();
+      var isFormValid = true; // Assumption
+      var validationsLeft = validationFns.length;
+
+      // In the case where there are no validtion fns.
+      if (!validationsLeft-- && isFormValid){
+        this.invoke('valid');
         return;
       }
 
-      var r = new JX.Request(this.getUri(), JX.bind(this,this._cbk));
-      r.setData();
-      r.send();
+      // Try validating the form now that we have validation fns
+      var isFormValidated = false;
+
+      for(var i=0, il = validationFns.length; i < il; i++){
+        var fn = validationFns[i];
+
+        //execute a validation fn
+        fn(function(result){
+          //dont invoke multiple 'invalid' events
+          // but invoke 'invalid' event the moment any validation fails
+          do {
+            isFormValid = isFormValid && result;
+            if (!isFormValid){
+              this.invoke('invalid');
+              isFormValidated = true;
+            }
+          } while (!isFormValidated);
+
+          if (!validationsLeft-- && isFormValid){
+            this.invoke('valid');
+          }
+        });
+
+      }
+    },
+    submit: function(){
+      this.invoke('start');
+      this.validate();
+      this.listen('valid', JX.bind(this,function(){
+        var r = new JX.Request(this.getUri(), JX.bind(this,this._cbk));
+        r.setData();
+        r.send();
+      }));
     }
   },
   properties: {
-    uri:null
+    uri:null,
+    validationFns:null
   }
 });
 
-
 JX.behavior('form', function(config, statics) {
   JX.Stratcom.listen('submit','form',function(e){
-    var data = e.getNodeData('form');
-    var target = e.getTarget();
-    var action = target.getAttribute('action') || data.uri;
     e.kill();
 
-    var f = new JX.Form(action);
+    var data = e.getNodeData('form');
+    var target = e.getTarget();
+    var action = target.getAttribute('action');
+    var validationFns = data.validationFns;
 
-    // // If an 'invalid' is received, disable the 'submit' button
-    // f.listen('invalid',function(){
-    //   target.setAttribute('disabled',true);
-    // })
+    var f = new JX.Form(action, validationFns);
+
     f.listen('done',data.onDone);
+    f.listen('invalid', data.onValidationFail);
     f.submit();
   });
 });
