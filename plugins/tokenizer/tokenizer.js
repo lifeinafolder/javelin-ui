656b3ce86611dc596e56cc57075165ff3d952494
Rajat Mittal <lifeinafolder@gmail.com>
2012-08-03 16:21:04 +0530

    [Tokenizer] Example

--- plugins/tokenizer/tokenizer.js
+++ plugins/tokenizer/tokenizer.js
@@ -3,24 +3,9 @@ JX.behavior('tokenizer', function(config) {
   var typeahead = new JX.Typeahead(JX.$(config.preloadedHardpoint));
 
   typeahead.setDatasource(source);
-  // typeahead.listen('choose', function(target) {
-  //   JX.$U(target.href).go();
-  // });
-  typeahead.setPlaceholder('Type an animal name...');
   typeahead.start();
 
   var tokenizer = new JX.Tokenizer(JX.$(config.preloadedHardpoint));
   tokenizer.setTypeahead(typeahead);
   tokenizer.start();
-
-  // var ondemandsource = new JX.TypeaheadOnDemandSource(config.ondemandURI);
-  // var ondemandtypeahead = new JX.Typeahead(JX.$(config.ondemandHardpoint));
-
-  // ondemandtypeahead.setDatasource(ondemandsource);
-  // ondemandtypeahead.listen('choose', function(target) {
-  //   JX.$U(target.href).go();
-  // });
-  // ondemandtypeahead.setPlaceholder('Type an animal name...');
-  // ondemandtypeahead.start();
-
 });
